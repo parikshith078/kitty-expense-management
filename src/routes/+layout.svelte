0b148c955cca81ed781d0f7013fb438b37614670
Parikshith Palegar <palegar.parikshith@gmail.com>
2024-06-28 12:47:56 +0530

    Added devtool to debug layouts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script>
+	import { DevTools, Visualize } from '$lib/devtools'
 	import '../app.css'
 </script>
 
-<main class="max-h-screen min-h-screen w-full bg-backgound font-inter text-black">
-	<slot />
-</main>
+<DevTools />
+
+<Visualize layout="Root">
+	<main class="max-h-screen min-h-screen w-full bg-backgound font-inter text-black">
+		<slot />
+	</main>
+</Visualize>
